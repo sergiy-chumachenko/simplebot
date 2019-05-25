fbbf89bc30ad05770c60e483857ba4e317fd92c9
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2019-05-25 19:41:23 +0300

    [UPDATE] BotHandler has been created

--- bot/main.py
+++ bot/main.py
@@ -1,58 +1,83 @@
 import requests
 import logging
-from time import sleep
-
-BOT_TOKEN = '<your_bot_token>'
-URL = f"https://api.telegram.org/bot{BOT_TOKEN}/"
+import os
+import datetime
 
 
-def get_last_updates():
-    response = requests.get(url=URL + 'getUpdates')
-    return response.json()
+class BotHandler(object):
+    def __init__(self, token):
+        self.token = token
+        self.api_url = f"https://api.telegram.org/bot{token}/"
 
+    def get_updates(self, offset=None, timeout=10):
+        method = 'getUpdates'
+        params = {'timeout': timeout, 'offset': offset}
+        response = requests.get(url=self.api_url + method, params=params)
+        return response.json()
 
-def last_update(data):
-    if data.get('ok'):
-        try:
-            last_update = data['result'][-1]
-        except (KeyError, IndexError) as e:
-            logging.error("Unable to get last result from data. %s", e)
+    def send_msg(self, chat_id, msg):
+        method = 'sendMessage'
+        params = {
+            'chat_id': chat_id,
+            'text': msg
+        }
+        response = requests.post(url=self.api_url + method, data=params)
+        return response
+
+    def get_last_update(self):
+        get_result = self.get_updates()
+        if get_result.get('ok'):
+            result = get_result.get('result')
+            if len(result) > 0:
+                return result[-1]
+            return
         else:
-            return last_update
-    else:
-        logging.error('Something goes wrong!')
-        raise requests.exceptions.RequestException
+            raise requests.RequestException
 
 
-def get_chat_id(update):
-    return update['message']['chat']['id']
-
-
-def send_msg(chat_id, msg):
-    params = {
-        'chat_id': chat_id,
-        'text': msg
-    }
-    response = requests.post(url=URL + 'sendMessage', data=params)
-    return response
+simple_bot = BotHandler(token=os.environ.get('TELEGRAM_BOT_TOKEN'))
+greetings = ("hi", "hello", "hi!", "hello!", "what's up?")
+now = datetime.datetime.now()
 
 
 def main():
-    try:
-        update_id = last_update(get_last_updates()).get('update_id')
-        while True:
-            update = last_update(get_last_updates())
-            last_upd_id = update.get('update_id')
-            logging.info(f"Last update id: {last_upd_id}")
-            if update_id == last_upd_id:
-                send_msg(chat_id=get_chat_id(update), msg='test')
-                update_id += 1
-            sleep(1)
-    except requests.exceptions.RequestException as e:
-        logging.error('Request Error! %s', e)
+    new_offset = None
+    today = now.day
+    hour = now.hour
+
+    while True:
+        try:
+            simple_bot.get_updates(new_offset)
+
+            last_update = simple_bot.get_last_update()
+            last_update_id = last_update['update_id']
+            last_chat_text = last_update['message']['text']
+            last_chat_id = last_update['message']['chat']['id']
+            last_chat_name = last_update['message']['chat']['first_name']
+        except (ValueError, requests.RequestException) as e:
+            logging.error("An error has been occurred! %s", e)
+        except TypeError:
+            logging.info("Updates has not been found for now!")
+        else:
+            if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
+                simple_bot.send_msg(last_chat_id, 'Good morning, {}'.format(last_chat_name))
+                today += 1
+
+            elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
+                simple_bot.send_msg(last_chat_id, 'Good afternoon, {}'.format(last_chat_name))
+                today += 1
+
+            elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
+                simple_bot.send_msg(last_chat_id, 'Good evening, {}'.format(last_chat_name))
+                # today += 1
+
+            new_offset = last_update_id + 1
 
 
 if __name__ == "__main__":
     logging.basicConfig(format=u'%(levelname)-8s [%(asctime)s] %(message)s', level=logging.INFO)
     logging.info('Start talking...')
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
